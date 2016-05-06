6a863454505b4306aa8daa50c366b7526a6ef0d6
AdrianMSS <sesamaua@gmail.com>
2016-05-06 02:52:28 -0600

    Commit Imaginev2 #1

---
+++ js/imagine.js
@@ -0,0 +1,188 @@
+/******* Imagine XYZ Functions *******/
+//Sprite Carousell
+function moveSprite(num){
+	var newPosition = num + "% 0%";
+	$("#partnersCarousell").css("background-position", newPosition);
+	num+=10;
+	setTimeout(moveSprite, 2000, num);
+};
+//Contact Events
+$(function(){
+	var contactName = false,
+		contactEmail = false,
+		contactPhone = false,
+		contactMessage = false,
+		nameVal = '',
+		emailVal = '',
+		phoneVal = '',
+		messageVal = '';
+
+    $(".contactButton").click(function(){
+        if(contactName && contactEmail && contactPhone && contactMessage){
+	        var url = 'email/',
+	            data = {
+	              'email': emailVal, 
+	              'name': nameVal,
+	              'message': messageVal,
+	              'phone': phoneVal
+	            };
+	        $.ajax({ 
+	            url: url,
+	            type: 'POST',
+	            data: JSON.stringify(data),
+	            beforeSend : function(req) { 
+	              req.setRequestHeader('content-type', 'application/json'); 
+	            },
+	            success: function(res){
+		            alert('Message Sent.');
+		            $('.contactName').val('');
+		            $('.contactEmail').val('');
+		            $('.contactPhone').val('');
+		            $('.contactMessage').val('');
+		            contactName = false,
+					contactEmail = false,
+					contactPhone = false,
+					contactMessage = false,
+					nameVal = '',
+					emailVal = '',
+					phoneVal = '',
+					messageVal = '';
+		    		$(".contactName").css("border-left", "black 1px solid");
+		    		$(".contactName").css("border-bottom", "black 1px solid");
+		    		$(".contactPhone").css("border-left", "black 1px solid");
+		    		$(".contactPhone").css("border-bottom", "black 1px solid");
+		    		$(".contactEmail").css("border-left", "black 1px solid");
+		    		$(".contactEmail").css("border-bottom", "black 1px solid");
+		    		$(".contactMessage").css("border", "black 2px solid");
+	            }.bind(this),
+	            error: function(res){
+	              	alert('Error: Message not Sent, Try Again.');
+	            }.bind(this)        
+	        });
+        }
+    });
+
+    $(".contactName").change(function(){
+    	var name = $( this ).val();
+    	if(name === ''){
+    		$(".contactName").css("border-left", "red 1px solid");
+    		$(".contactName").css("border-bottom", "red 1px solid");
+    		contactName = false;
+    	}
+    	else if(/[0-9]/.test(name)){
+    		$(".contactName").css("border-left", "red 1px solid");
+    		$(".contactName").css("border-bottom", "red 1px solid");
+    		contactName = false;
+    	}
+    	else {
+    		$(".contactName").css("border-left", "green 1px solid");
+    		$(".contactName").css("border-bottom", "green 1px solid");
+    		contactName = true;
+    		nameVal = name;
+    	}
+    });
+
+    $(".contactEmail").change(function(){
+    	var mail = $( this ).val();
+    	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	if(mail === ''){
+    		$(".contactEmail").css("border-left", "red 1px solid");
+    		$(".contactEmail").css("border-bottom", "red 1px solid");
+    		contactEmail = false;
+    	}
+    	else if(re.test(mail)){
+    		$(".contactEmail").css("border-left", "green 1px solid");
+    		$(".contactEmail").css("border-bottom", "green 1px solid");
+    		contactEmail = true;
+    		emailVal = mail;
+    	}
+    	else {
+    		$(".contactEmail").css("border-left", "red 1px solid");
+    		$(".contactEmail").css("border-bottom", "red 1px solid");
+    		contactEmail = false;
+    	}
+    });
+
+    $(".contactPhone").change(function(){
+    	var phone = $( this ).val();
+    	var re = /^[\s()+-]*([0-9][\s()+-]*){6,20}$/;
+    	if(phone === ''){
+    		$(".contactPhone").css("border-left", "red 1px solid");
+    		$(".contactPhone").css("border-bottom", "red 1px solid");
+    		contactPhone = false;
+    	}
+    	else if(re.test(phone)){
+    		$(".contactPhone").css("border-left", "green 1px solid");
+    		$(".contactPhone").css("border-bottom", "green 1px solid");
+    		contactPhone = true;
+    		phoneVal = phone;
+    	}
+    	else {
+    		$(".contactPhone").css("border-left", "red 1px solid");
+    		$(".contactPhone").css("border-bottom", "red 1px solid");
+    		contactPhone = false;
+    	}
+    });
+
+    $(".contactMessage").change(function(){
+    	var message = $( this ).val();
+    	if(message === ''){
+    		$(".contactMessage").css("border-left", "red 1px solid");
+    		$(".contactMessage").css("border-bottom", "red 1px solid");
+    		contactMessage = false;
+    	}
+    	else {
+    		$(".contactMessage").css("border", "green 2px solid");
+    		contactMessage = true;
+    		messageVal = message;
+    	}
+    });
+
+    //Subscribe Events
+    var susbscribeEmail = false,
+        emailVal2 = '';
+    $(".susbscribeEmail").change(function(){
+        var mail = $( this ).val();
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(mail === ''){
+            $(".susbscribeEmail").css("border", "red 1px solid");
+            susbscribeEmail = false;
+        }
+        else if(re.test(mail)){
+            $(".susbscribeEmail").css("border", "green 1px solid");
+            susbscribeEmail = true;
+            emailVal2 = mail;
+        }
+        else {
+            $(".susbscribeEmail").css("border", "red 1px solid");
+            susbscribeEmail = false;
+        }
+    });
+
+    $(".susbscribeButton").click(function(){
+        if(susbscribeEmail){
+            var url = 'susbscribe/',
+                data = {
+                  'email': emailVal2
+                };
+            $.ajax({ 
+                url: url,
+                type: 'POST',
+                data: JSON.stringify(data),
+                beforeSend : function(req) { 
+                  req.setRequestHeader('content-type', 'application/json'); 
+                },
+                success: function(res){
+                    alert('Message Sent.');
+                    $('.susbscribeEmail').val('');
+                    susbscribeEmail = false,
+                    emailVal2 = '',
+                    $(".susbscribeEmail").css("border", "black 1px solid");
+                }.bind(this),
+                error: function(res){
+                    alert('Error: Message not Sent, Try Again.');
+                }.bind(this)        
+            });
+        }
+    });
+});
--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 var express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    cookieSession = require('cookie-session');
+    nodemailer = require('nodemailer');
  
 var app = express();
 
@@ -15,6 +15,60 @@ var app = express();
 app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
 app.use(bodyParser());
 app.use(express.static(__dirname));
+
+var generator = require('xoauth2').createXOAuth2Generator({
+    user: 'adriansanchez.logn',
+    clientId: '980433155755-adpdvti4k47c8elkor63181u34e71u0m.apps.googleusercontent.com',
+    clientSecret: 'Wrx6qdcv2l4AqRj0X7eJUqRf',
+    refreshToken: '1/kfGGuqSGP7q2TEKkPBHXHkN8H3Km9AjTF9Nqg-OiYUc',
+    accessToken: 'ya29.swHEs_vuQz2ZWDxkVJI1gpTMOKiFTVquopDydk_SVLoUcUGeBFxpNGAgfeVhRPstOtmj' // optional
+});
+
+generator.on('token', function(token){
+});
+
+var transporter = nodemailer.createTransport(({
+    service: 'gmail',
+    auth: {
+        xoauth2: generator
+    }
+}));
+
+app.post('/email/', function (req, res) {
+    var fecha = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+    mailOptions = {
+    to: 'sesamaua@gmail.com', // receiver
+     subject: 'ImagineXYZ: Desde la pagina web - Fecha: ' + fecha, // subject
+     text: 'Email: ' + req.body['email'] + '. \n'+ 'Name: ' + req.body['name'] + '. \n'+ 'Phone: ' + req.body['phone'] + '. \n'  + 'Message: ' + req.body['message'] // body
+     };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.send(400);
+    }else{
+      console.log('Message sent: ' + info.response);
+      res.send(200);
+    }
+  });
+});
+
+app.post('/susbscribe/', function (req, res) {
+    var fecha = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+    mailOptions = {
+    to: 'sesamaua@gmail.com', // receiver
+     subject: 'ImagineXYZ: Susbcripción a la pagina web - Fecha: ' + fecha, // subject
+     text: 'Email: ' + req.body['email'] // body
+     };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.send(400);
+    }else{
+      console.log('Message sent: ' + info.response);
+      res.send(200);
+    }
+  });
+});
 
 app.get('*', function (req, res) {
     res.redirect('../#home', 404);

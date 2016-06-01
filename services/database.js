421db2fafafbf8b55bd7ea6535a602cd9f8d3baa
AdrianMSS <sesamaua@gmail.com>
2016-06-01 12:36:45 -0600

    Commit Imagine Web v2 URI

--- services/database.js
+++ services/database.js
@@ -9,8 +9,9 @@ var mongoose = require ("mongoose"); // The reason for this demo.
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.  
 var uristring = 
-  process.env.MONGOLAB_URI || 
+  process.env.MONGODB_URI || 
   process.env.MONGOHQ_URL || 
+  process.env.MONGOLAB_URI||
   'mongodb://localhost/Hardwarethon';
 
 

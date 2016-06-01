f21311e26eb5b4c0dd8192332a3b548652c52621
AdrianMSS <sesamaua@gmail.com>
2016-06-01 14:07:36 -0600

    Commit Imagine v2 Clean

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var express = require('express'),
     cors = require('cors');
 
 //REST APIS
-var  organizationImagine = require('./services/imagine'),
+/*var  organizationImagine = require('./services/imagine'),
     database = require('./services/database'),
     googleApi = require('./services/google');
-
+*/
 /*
 Seconds: 0-59
 Minutes: 0-59
@@ -33,7 +33,7 @@ app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
 app.use(bodyParser());
 //app.use('/hwthon2015', express.static(__dirname + '/hwthon2015'));
 app.use(express.static(__dirname + '/webpage'));
-app.use('/whatsnear', express.static(__dirname + '/whatsnear'));
+/*app.use('/whatsnear', express.static(__dirname + '/whatsnear'));
 app.use('/javisstops', express.static(__dirname + '/javisstops'));
 app.use('/grades', express.static(__dirname + '/grades'));
 
@@ -46,169 +46,10 @@ app.get('/whatsnear/pos', organizationImagine.getData);
 app.get('/javisstops/pos', organizationImagine.getData);
 app.get('/rentacar/pos', organizationImagine.getData);
 app.post('/rentacar/pos', organizationImagine.newData);
+*/
 
 
-/*app.post('/ElectronicsSolutionDevices/', function(res,req){
-  database.newData(res,req,'ElectronicsSolutionDevices');
-});
-app.get('/ElectronicsSolutionDevices/', function(res,req){
-  database.getData(res,req,'ElectronicsSolutionDevices');
-});
-
-    
-app.post('/OvejasElectricas/', function(res,req){
-  database.newData(res,req,'OvejasElectricas');
-});
-app.get('/OvejasElectricas/', function(res,req){
-  database.getData(res,req,'OvejasElectricas');
-});
-
-    
-app.post('/Bulldozer/', function(res,req){
-  database.newData(res,req,'Bulldozer');
-});
-app.get('/Bulldozer/', function(res,req){
-  database.getData2(res,req,'Bulldozer');
-});
-
-    
-app.post('/SmarTicos/', function(res,req){
-  database.newData(res,req,'SmarTicos');
-});
-app.get('/SmarTicos/', function(res,req){
-  database.getData(res,req,'SmarTicos');
-});
-
-    
-app.post('/InnovationSourceCode/', function(res,req){
-  database.newData(res,req,'InnovationSourceCode');
-});
-app.get('/InnovationSourceCode/', function(res,req){
-  database.getData(res,req,'InnovationSourceCode');
-});
-
-    
-app.post('/Envitech/', function(res,req){
-  database.newData(res,req,'Envitech');
-});
-app.get('/Envitech/', function(res,req){
-  database.getData(res,req,'Envitech');
-});
-
-    
-app.post('/Neotronic/', function(res,req){
-  database.newData(res,req,'Neotronic');
-});
-app.get('/Neotronic/', function(res,req){
-  database.getData(res,req,'Neotronic');
-});
-
-   
-app.post('/InfotronicCircuits/', function(res,req){
-  database.newData(res,req,'InfotronicCircuits');
-});
-app.get('/InfotronicCircuits/', function(res,req){
-  database.getData2(res,req,'InfotronicCircuits');
-});
-
-    
-app.post('/Float/', function(res,req){
-  database.newData(res,req,'Float');
-});
-app.get('/Float/', function(res,req){
-  database.getData(res,req,'Float');
-});
-
-    
-app.post('/InDePro/', function(res,req){
-  database.newData(res,req,'InDePro');
-});
-app.get('/InDePro/', function(res,req){
-  database.getData(res,req,'InDePro');
-});
-
-    
-app.post('/LaNaranjaMecanica/', function(res,req){
-  database.newData(res,req,'LaNaranjaMecanica');
-});
-app.get('/LaNaranjaMecanica/', function(res,req){
-  database.getData(res,req,'LaNaranjaMecanica');
-});
-
-    
-app.post('/FrozenbyteKnights/', function(res,req){
-  database.newData(res,req,'FrozenbyteKnights');
-});
-app.get('/FrozenbyteKnights/', function(res,req){
-  database.getData(res,req,'FrozenbyteKnights');
-});
-
-    
-app.post('/Iwa/', function(res,req){
-  database.newData(res,req,'Iwa');
-});
-app.get('/Iwa/', function(res,req){
-  database.getData(res,req,'Iwa');
-});
-
-    
-app.post('/TeamCR/', function(res,req){
-  database.newData(res,req,'TeamCR');
-});
-app.get('/TeamCR/', function(res,req){
-  database.getData(res,req,'TeamCR');
-});
-
-    
-app.post('/NovaMakers/', function(res,req){
-  database.newData(res,req,'NovaMakers');
-});
-app.get('/NovaMakers/', function(res,req){
-  database.getData(res,req,'NovaMakers');
-});
-
-
-app.post('/A/', function(res,req){
-  database.newData(res,req,'A');
-});
-app.get('/A/', function(res,req){
-  database.getData(res,req,'A');
-});
-
-    
-app.post('/WIN/', function(res,req){
-  database.newData(res,req,'WIN');
-});
-app.get('/WIN/', function(res,req){
-  database.getData(res,req,'WIN');
-});
-
-    
-app.post('/X/', function(res,req){
-  database.newData(res,req,'X');
-});
-app.get('/X/', function(res,req){
-  database.getData(res,req,'X');
-});
-
-    
-app.post('/Y/', function(res,req){
-  database.newData(res,req,'Y');
-});
-app.get('/Y/', function(res,req){
-  database.getData(res,req,'Y');
-});
-
-    
-app.post('/Z/', function(res,req){
-  database.newData(res,req,'Z');
-});
-app.get('/Z/', function(res,req){
-  database.getData(res,req,'Z');
-});*/
-
-
-var generator = require('xoauth2').createXOAuth2Generator({
+/*var generator = require('xoauth2').createXOAuth2Generator({
     user: 'adriansanchez.logn',
     clientId: '980433155755-adpdvti4k47c8elkor63181u34e71u0m.apps.googleusercontent.com',
     clientSecret: 'Wrx6qdcv2l4AqRj0X7eJUqRf',
@@ -242,7 +83,7 @@ app.post('/email/', function (req, res) {
       res.send(200);
     }
   });
-});
+});*/
 
 app.get('*', function (req, res) {
     res.redirect('../#home', 404);
